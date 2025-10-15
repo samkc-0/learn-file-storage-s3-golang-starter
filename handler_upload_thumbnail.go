53fe517a43f93de3aa548a9279601cc481f379c0
samkc-0 <sam.k.c.0@icloud.com>
2025-10-15 09:38:02 +0100

    remove get thumbnail endpoint

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +31,36 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	tenMegabytes := int64(10 << 20)
+	r.ParseMultipartForm(tenMegabytes)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	data, err := io.ReadAll(file)
+	metadata, err := cfg.db.GetVideo(videoID)
+	if metadata.UserID != userID {
+		err := errors.New("unauthorized")
+		respondWithError(w, http.StatusUnauthorized, "bad credentials", err)
+		return
+	}
+	dataString := base64.StdEncoding.EncodeToString(data)
+	dataUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, dataString)
+	metadata.ThumbnailURL = &dataUrl
+	err = cfg.db.UpdateVideo(metadata)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, metadata)
+
 }
